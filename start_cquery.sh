739f62ca2919d56600151189e19397dfdd54536f
ganimaedes <41844819+ganimaedes@users.noreply.github.com>
2018-08-21 20:15:23 -0400

    Update start_cquery.sh

--- start_cquery.sh
+++ start_cquery.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
-cquery_folder=`find "/home/$(id -u -n)" -type d -iname "cquery" 2>/dev/null`
-"$cquery_folder"/build/cquery --log-file /tmp/cquery_log.txt --init='{
+cquery_folder=`find "/home/$(id -u -n)" -type d -wholename "*cquery/build" 2>/dev/null`
+"$cquery_folder"/cquery --log-file /tmp/cquery_log.txt --init='{
   "cacheDirectory": "/tmp/cquery",
   "progressReportFrequencyMs": -1
 }'
